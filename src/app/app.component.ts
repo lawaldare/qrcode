d4b0070988d0172c83f2243011c2851c3ab9aa7a
Dare Lawal <lawaldare@gmail.com>
2022-08-28 08:28:43 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,6 @@ import { FormBuilder, Validators } from '@angular/forms';
 
 import * as QRCode from 'qrcode'
 
-
-interface Size {
-  label: string;
-  value: number;
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -21,38 +15,6 @@ export class AppComponent {
   })
 
   urlGenerated: string = '';
-
-  sizeOptions: Size[] = [
-    {
-      label: '100x100',
-      value: 100
-    },
-    {
-      label: '200x200',
-      value: 200
-    },
-    {
-      label: '300x300',
-      value: 300
-    },
-    {
-      label: '400x400',
-      value: 400
-    },
-    {
-      label: '500x500',
-      value: 500
-    },
-    {
-      label: '600x600',
-      value: 600
-    },
-    {
-      label: '700x700',
-      value: 700
-    }
-  ]
-
 
   constructor(private fb: FormBuilder) { }
 
@@ -78,6 +40,9 @@ export class AppComponent {
           console.error(err)
         })
 
+    } else {
+      alert('Please enter a valid URL');
+      return;
     }
   }
 
